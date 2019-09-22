f35ed86f1762c9af7abc24a7af993dbe149a0eb1
hannahhu <hannahhu@umich.edu>
2019-09-22 19:11:02 -0400

    top.v

--- top.v
+++ top.v
@@ -7,13 +7,13 @@
 `define WEIGHT_SIZE 32
 
 module top (
-	input clk,    // Clock
-	input rst,  	// Asynchronous reset active high
-	output [3:0] result
+	input wire clk,    // Clock
+	input wire rst,  	// Asynchronous reset active high
+	output reg [3:0] result
 );
 
 // read_data
-	wire [`DATA_SIZE-1:0] data [`DATA_X-1:0][`DATA_Y-1:0], // 28*28 32-bit data
+	wire [`DATA_SIZE-1:0] data [`DATA_X-1:0][`DATA_Y-1:0]; // 28*28 32-bit data
 	wire signed [`WEIGHT_SIZE-1:0] weight_1 [`WEIGHT_X-1:0][`WEIGHT_Y-1:0];
 	wire signed [`WEIGHT_SIZE-1:0] weight_2 [`WEIGHT_X-1:0][`WEIGHT_Y-1:0];
 	wire signed [`WEIGHT_SIZE-1:0] weight_3 [`WEIGHT_X-1:0][`WEIGHT_Y-1:0];
@@ -80,9 +80,37 @@ module top (
 	wire [31:0] prob_7;
 	wire [31:0] prob_8;
 	wire [31:0] prob_9;
+	wire fc_done;
 
-// TODO: read_data
+// read_data
+	read_data read_data(
+		// INPUTS
+		.clk(clk),
+	 	.rst(rst),
 
+		// OUTPUTS
+		.data(data),
+		.weight_1(weight_1),
+		.weight_2(weight_2),
+		.weight_3(weight_3),
+		.weight_4(weight_4),
+		.weight_5(weight_5),
+		.weight_6(weight_6),
+		.weight_7(weight_7),
+		.weight_8(weight_8),
+		.fc_weight_0(fc_weight_0),
+		.fc_weight_1(fc_weight_1),
+		.fc_weight_2(fc_weight_2),
+		.fc_weight_3(fc_weight_3),
+		.fc_weight_4(fc_weight_4),
+		.fc_weight_5(fc_weight_5),
+		.fc_weight_6(fc_weight_6),
+		.fc_weight_7(fc_weight_7),
+		.fc_weight_8(fc_weight_8),
+		.fc_weight_9(fc_weight_9)
+	);
+
+// TODO: conv_enable control signal
 
 // conv_layer
 	conv_layer conv_layer(
@@ -203,16 +231,73 @@ module top (
 			.fc_done(fc_done)
 		);
 
-// TODO: compare prob0-prob9 and get result
+// Compare prob0-prob9 and get result
 
+	reg [31:0] in_prob_0, in_prob_1, in_prob_2, in_prob_3, in_prob_4, in_prob_5, in_prob_6, in_prob_7, in_prob_8, in_prob_9;
 
+	always @ (fc_done) begin
+		in_prob_0 <= prob_0;
+		in_prob_1 <= prob_1;
+		in_prob_2 <= prob_2;
+		in_prob_3 <= prob_3;
+		in_prob_4 <= prob_4;
+		in_prob_5 <= prob_5;
+		in_prob_6 <= prob_6;
+		in_prob_7 <= prob_7;
+		in_prob_8 <= prob_8;
+		in_prob_9 <= prob_9;
+	end
 
+	integer i, j;
+	reg [31:0] prob [9:0];
+	reg [31:0] tmp_prob;
 
+	always @ (*) begin
+		prob[0] = in_prob_0;
+		prob[1] = in_prob_1;
+		prob[2] = in_prob_2;
+		prob[3] = in_prob_3;
+		prob[4] = in_prob_4;
+		prob[5] = in_prob_5;
+		prob[6] = in_prob_6;
+		prob[7] = in_prob_7;
+		prob[8] = in_prob_8;
+		prob[9] = in_prob_9;
 
+		for (i = 9; i >= 0; i = i-1) begin
+			for (j = 0; j < i; j = j+1) begin
+				if (prob[j] < prob[j+1]) begin
+					tmp_prob = prob[j];
+					prob[j] = prob[j+1];
+					prob[j+1] = tmp_prob;
+				end
+			end
+		end
+	end
 
-
-
-
-
+	always @ (posedge clk) begin
+		if (prob[9] = prob_0)
+			result = 4'd0;
+		else if (prob[9] = prob_1)
+			result = 4'd1;
+		else if (prob[9] = prob_2)
+			result = 4'd2;
+		else if (prob[9] = prob_3)
+			result = 4'd3;
+		else if (prob[9] = prob_4)
+			result = 4'd4;
+		else if (prob[9] = prob_5)
+			result = 4'd5;
+		else if (prob[9] = prob_6)
+			result = 4'd6;
+		else if (prob[9] = prob_7)
+			result = 4'd7;
+		else if (prob[9] = prob_8)
+			result = 4'd8;
+		else if (prob[9] = prob_9)
+			result = 4'd9;
+		else
+			result = 4'd16;
+	end
 
 endmodule
